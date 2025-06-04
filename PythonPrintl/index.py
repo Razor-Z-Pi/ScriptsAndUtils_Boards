b1ffd09e4cef297b03365d491a7a5f070129a0f1
Razor-Z-Pi <popovp.popoff2001@gmail.com>
2025-06-04 23:10:46 +0700

    update script py, +parser

--- PythonPrintl/index.py
+++ PythonPrintl/index.py
@@ -2,6 +2,7 @@ import serial
 import sqlite3
 import json
 import time
+import re
 
 con = sqlite3.connect("pop.db")
 cur = con.cursor()
@@ -21,19 +22,27 @@ if not cur.fetchone():
 else:
     print("Таблица уже существует")
 
+cur.execute("SELECT * FROM tempii")
+record = cur.fetchall()
+print(f"Всего строк {len(record)}")
+for row in record:
+	print(f"ID: {row[0]}")
+	print(f"t: {row[1]}")
+	print(f"Name: {row[2]}")
+
 # Работа с последовательным портом
 try:
     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as ser:
         print("Подключение к последовательному порту...")
         while True:
             if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').strip()
+                line = ser.readline().decode('utf-8')
                 try:
-                    data = json.loads(line)
-                    if isinstance(data, dict) and 't' in data and 'id' in data:
-                        cur.execute("INSERT INTO tempii (t, name) VALUES (?, ?)", (data['t'], data['id']))
+                    t = re.split(r"['':,\s]", line)
+                    if len(t) > 0:
+                        cur.execute("INSERT INTO tempii (t, name) VALUES (?, ?)", (t[4], t[10]))
                         con.commit()
-                        print(f"Данные записаны в БД: t={data['t']}, name={data['id']}")
+                        print(f"Данные записаны в БД")
                     else:
                         print("Некорректный формат данных:", line)
                 except json.JSONDecodeError:
